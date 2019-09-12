69919173a8327a0920f74722449e7184563ebfdd
Johnson Zhong <johnsonz8642@gmail.com>
2019-09-12 10:58:20 -0400

    Added routing. Transfered home text. Moved css.

--- portfolio-site/src/app/app-routing.module.ts
+++ portfolio-site/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio-site/src/app/app.component.ts
+++ portfolio-site/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio-site';
+  // title = 'Johnson\'s Portfolio Website';
+  title = 'test';
 }
