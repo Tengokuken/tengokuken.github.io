daba4dd0f480845329fa3a39a19631ca2062dd48
Johnson Zhong <johnsonz8642@gmail.com>
2019-09-12 11:06:29 -0400

    Fixed routing to add projects link.

--- portfolio-site/src/app/app-routing.module.ts
+++ portfolio-site/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
 ];
 
